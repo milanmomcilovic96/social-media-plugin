482632a7baa8ae59673774cd56b03a1e091cd85d
unknown <milanmomcilovic96@gmail.com>
2024-01-09 13:55:58 +0100

    Added Social Share page

--- social-media-share.php
+++ social-media-share.php
@@ -7,11 +7,28 @@
  **/
 
 
- 
+// Sidebar Menu - Social Media Share 
 function social_media_share() {
     add_menu_page('Social Media Share', 'Social Media Share', 'manage_options', 'social-media-share', 'social_share_page', '', 200);
 }
 add_action('admin_menu', 'social_media_share');
+
+// Social Media Share Page
+function social_share_page() {
+    $plugin_title = get_admin_page_title();
+    ?>
+<div class="wrap">
+    <h1><?php print $plugin_title; ?></h1>
+    <form method="post" action="options.php">
+        <?php
+            settings_fields("social_share_config_section");
+            submit_button();
+            ?>
+    </form>
+</div>
+<?php
+}
+
 
 
 ?>
