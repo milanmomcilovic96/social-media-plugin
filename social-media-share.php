5f68811d46579a77d92b9bfe873b01e3e670931a
unknown <milanmomcilovic96@gmail.com>
2024-01-09 12:09:45 +0100

    Initial commit

---
+++ social-media-share.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Social Media Share
+ * Description: Social Media Share plugin for WordPress, created for LibraFire test assignment.
+ * Version: 1.0
+ * Author: Milan Momcilovic
+ **/
+
+
+?>
