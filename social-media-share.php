7abb26dc8e6fa68fe366e1ae604c2cc00f04dee9
unknown <milanmomcilovic96@gmail.com>
2024-01-12 23:08:50 +0100

    Refactored code and fixed minor bugs, fixed style, added tabs.

--- social-media-share.php
+++ social-media-share.php
@@ -6,8 +6,8 @@
  * Author: Milan Momcilovic
  **/
 
-// Enqueue styles and scripts
 function enqueue_social_share_styles() {
+    wp_enqueue_script('jquery');
     wp_enqueue_style('wp-color-picker');
     wp_enqueue_script('wp-color-picker');
     $css_file = plugin_dir_path(__FILE__) . 'style.css';
@@ -16,34 +16,48 @@ function enqueue_social_share_styles() {
 
 add_action('wp_enqueue_scripts', 'enqueue_social_share_styles');
 
-// Add admin menu
 function social_media_share() {
     add_menu_page('Social Media Share', 'Social Media Share', 'manage_options', 'social-media-share', 'social_share_page', '', 200);
 }
 
 add_action('admin_menu', 'social_media_share');
-
-// Display settings page
 function social_share_page() {
+    $active_tab = isset($_GET['tab']) ? sanitize_text_field($_GET['tab']) : 'general';
     $plugin_title = get_admin_page_title();
     ?>
 <div class="wrap">
     <h1><?php echo esc_html($plugin_title); ?></h1>
+
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=social-media-share&tab=general"
+            class="nav-tab <?php echo ($active_tab === 'general') ? 'nav-tab-active' : ''; ?>">General</a>
+        <a href="?page=social-media-share&tab=style"
+            class="nav-tab <?php echo ($active_tab === 'style') ? 'nav-tab-active' : ''; ?>">Style</a>
+        <a href="?page=social-media-share&tab=display"
+            class="nav-tab <?php echo ($active_tab === 'display') ? 'nav-tab-active' : ''; ?>">Display</a>
+    </h2>
+
     <form method="post" action="options.php">
         <?php
-            settings_fields("social_share_config_section");
-            do_settings_sections("social-share");
-            do_settings_sections("social-share-display");
+            if ($active_tab === 'general') {
+                settings_fields("social_share_config_section_general");
+                do_settings_sections("social-share-general");
+            } elseif ($active_tab === 'style') {
+                settings_fields("social_share_config_section_style");
+                do_settings_sections("social-share-style");
+            } elseif ($active_tab === 'display') {
+                settings_fields("social_share_config_section_display");
+                do_settings_sections("social-share-display");
+            }
             submit_button();
             ?>
     </form>
 </div>
 <?php
 }
-
-// Define settings and sections
 function social_share_settings() {
-    $custom_post_types = get_post_types(array('_builtin' => false));
+    add_settings_section("social_share_config_section_general", "General Settings", null, "social-share-general");
+
     $social_networks = array(
         'facebook' => 'Facebook',
         'twitter' => 'Twitter',
@@ -53,59 +67,80 @@ function social_share_settings() {
         'whatsapp' => 'WhatsApp (Mobile only)'
     );
 
-    // Social media options
-    add_settings_section("social_share_config_section", "Social Share Settings", null, "social-share");
-
     foreach ($social_networks as $network_key => $network_label) {
-        add_settings_field("social-share-{$network_key}", $network_label, "social_share_checkbox", "social-share", "social_share_config_section", $network_key);
-        register_setting("social_share_config_section", "social-share-{$network_key}");
+        add_settings_field(
+            "social-share-{$network_key}",
+            $network_label,
+            "social_share_checkbox",
+            "social-share-general",
+            "social_share_config_section_general",
+            $network_key
+        );
+        register_setting("social_share_config_section_general", "social-share-{$network_key}");
     }
 
-    // Display settings
-    add_settings_section("social_share_display_section", "Display", null, "social-share-display");
+    add_settings_section("social_share_button_size_section", "Button Sizes", null, "social-share-style");
+    add_settings_field("social-share-button-size", "Button Size", "social_share_button_size_dropdown", "social-share-style", "social_share_button_size_section");
+    register_setting("social_share_config_section_style", "social-share-button-size");
 
-    add_settings_field("social-share-display-post", "Posts", "social_share_display_checkbox", "social-share-display", "social_share_display_section", 'display-post');
-    register_setting("social_share_config_section", "social-share-display-post");
+    add_settings_section("social_share_button_color_section", "Button Colors", null, "social-share-style");
+    register_setting('social_share_config_section_style', 'social_share_button_colors');
 
-    add_settings_field("social-share-display-page", "Pages", "social_share_display_checkbox", "social-share-display", "social_share_display_section", 'display-page');
-    register_setting("social_share_config_section", "social-share-display-page");
-
-    if ($custom_post_types) {
-        foreach ($custom_post_types as $post_type) {
-            add_settings_field("social-share-display-{$post_type}", ucfirst($post_type), "social_share_display_checkbox", "social-share-display", "social_share_display_section", "display-{$post_type}");
-            register_setting("social_share_config_section", "social-share-display-{$post_type}");
-        }
-    }
-
-    // Button size section
-    add_settings_section("social_share_button_size_section", "Button Sizes", null, "social-share");
-    add_settings_field("social-share-button-size", "Button Size", "social_share_button_size_dropdown", "social-share", "social_share_button_size_section");
-    register_setting("social_share_config_section", "social-share-button-size");
-
-    // Button color section
-    add_settings_section("social_share_button_color_section", "Button Colors", null, "social-share");
-    register_setting('social_share_config_section', 'social_share_button_colors');
+    $social_networks = array(
+        'facebook' => 'Facebook',
+        'twitter' => 'Twitter',
+        'google' => 'Google+',
+        'pinterest' => 'Pinterest',
+        'linkedin' => 'LinkedIn',
+        'whatsapp' => 'WhatsApp (Mobile only)'
+    );
 
     foreach ($social_networks as $network_key => $network_label) {
         add_settings_field(
-            "social_share_button_color_$network_key",
-            "$network_label Button Color",
+            "social-share-button-color-$network_key",
+            "$network_label Color",
             "display_button_color_field",
-            "social-share",
+            "social-share-style",
             "social_share_button_color_section",
             array('network_key' => $network_key)
         );
+        register_setting("social_share_config_section_style", "social-share-button-color-$network_key");
     }
 
-    // Display position
-    add_settings_section("social_share_display_position_section", "Display Position", null, "social-share");
-    add_settings_field("social-share-display-position", "Select Display Position", "social_share_display_position_dropdown", "social-share", "social_share_display_position_section");
-    register_setting("social_share_config_section", "social-share-display-position");
+    add_settings_section("social_share_display_position_section", "Display Position", null, "social-share-display");
+    add_settings_field("social-share-display-position", "Select Display Position", "social_share_display_position_dropdown", "social-share-display", "social_share_display_position_section");
+    register_setting("social_share_config_section_display", "social-share-display-position");
+
+    add_settings_section("social_share_display_on_section", "Display On", null, "social-share-display");
+
+    add_settings_field("social-share-display-post", "Posts", "social_share_display_checkbox", "social-share-display", "social_share_display_on_section", 'display-post');
+    register_setting("social_share_config_section_display", "social-share-display-post");
+
+    add_settings_field("social-share-display-page", "Pages", "social_share_display_checkbox", "social-share-display", "social_share_display_on_section", 'display-page');
+    register_setting("social_share_config_section_display", "social-share-display-page");
+
+    $custom_post_types = get_post_types(array('_builtin' => false));
+    if ($custom_post_types) {
+        foreach ($custom_post_types as $post_type) {
+            add_settings_field("social-share-display-{$post_type}", ucfirst($post_type), "social_share_display_checkbox", "social-share-display", "social_share_display_on_section", "display-{$post_type}");
+            register_setting("social_share_config_section_display", "social-share-display-{$post_type}");
+        }
+    }
 }
 
 add_action("admin_init", "social_share_settings");
 
-// Display button color field
+function social_share_icons_shortcode() {
+    global $post;
+
+    $url = esc_url(get_permalink($post->ID));
+    $social_media_links = generate_social_share_links($url);
+
+    return $social_media_links;
+}
+
+add_shortcode('social_share_icons', 'social_share_icons_shortcode');
+
 function display_button_color_field($args) {
     $network_key = $args['network_key'];
     $button_colors = get_option("social_share_button_colors");
@@ -126,7 +161,6 @@ function display_button_color_field($args) {
 <?php
 }
 
-// Display button size dropdown
 function social_share_button_size_dropdown() {
     $option_name = "social-share-button-size";
     $selected = get_option($option_name);
@@ -139,7 +173,6 @@ function social_share_button_size_dropdown() {
 <?php
 }
 
-// Display checkbox for social share options
 function social_share_checkbox($args) {
     $option_name = "social-share-{$args}";
     ?>
@@ -148,7 +181,6 @@ function social_share_checkbox($args) {
 <?php
 }
 
-// Display checkbox for social share display options
 function social_share_display_checkbox($args) {
     $option_name = "social-share-{$args}";
     ?>
@@ -157,7 +189,6 @@ function social_share_display_checkbox($args) {
 <?php
 }
 
-// Display position dropdown
 function social_share_display_position_dropdown() {
     $option_name = "social-share-display-position";
     $selected = get_option($option_name);
@@ -172,7 +203,6 @@ function social_share_display_position_dropdown() {
 <?php
 }
 
-// Generate social share links
 function generate_social_share_links($url) {
     $button_size = get_option("social-share-button-size");
     $button_colors = get_option("social_share_button_colors");
@@ -185,16 +215,18 @@ function generate_social_share_links($url) {
         'whatsapp' => 'https://api.whatsapp.com/send?text='
     );
 
-    $html = "<div class='social-share-wrapper {$button_size}'><div class='share-on'>Share on: </div>";
+    $html = "<div class='social-share-wrapper'>";
     foreach ($social_networks as $network_key => $network_url) {
         $button_color = '';
 
+         if ($network_key === 'whatsapp' && !wp_is_mobile()) {
+            continue;
+        }
+
         if (get_option("social-share-{$network_key}") == 1) {
             if (isset($button_colors[$network_key]['original']) && $button_colors[$network_key]['original'] == 1) {
-                // Use original color
                 $button_color = '';
             } elseif (isset($button_colors[$network_key]['color'])) {
-                // Use custom color
                 $button_color = "style='background-color: {$button_colors[$network_key]['color']}'";
             }
 
@@ -207,18 +239,17 @@ function generate_social_share_links($url) {
     return $html;
 }
 
-// Add social share icons to content
 function add_social_share_icons($content) {
     global $post;
 
     $url = esc_url(get_permalink($post->ID));
     $social_media_links = generate_social_share_links($url);
 
-    $displayed_content = ''; // Variable to store the displayed content
+    $displayed_content = ''; 
 
     $display_on_posts = get_option('social-share-display-post');
     $display_on_pages = get_option('social-share-display-page');
-
+    
     if (is_singular('post') && $display_on_posts) {
         $displayed_content .= $social_media_links;
     } elseif (is_singular('page') && $display_on_pages) {
@@ -226,20 +257,18 @@ function add_social_share_icons($content) {
     } elseif (is_singular()) {
         $custom_post_types = get_post_types(['_builtin' => false]);
         if (isset($custom_post_types) && is_array($custom_post_types)) {
-            // Loop through custom post types to check and display if enabled
             foreach ($custom_post_types as $post_type) {
                 $display_custom = get_option("social-share-display-{$post_type}");
                 if (is_singular($post_type) && $display_custom) {
                     $displayed_content .= $social_media_links;
-                    break; // Display only once for a custom post type
+                    break; 
                 }
             }
         }
     }
-
+    
     $display_position = get_option('social-share-display-position');
 
-    // Append the social links based on the selected display position
     switch ($display_position) {
         case 'below-title':
             $content = $displayed_content . $content;
@@ -247,22 +276,50 @@ function add_social_share_icons($content) {
         case 'after-content':
             $content .= $displayed_content;
             break;
-        case 'floating-left':
-            $content .= $displayed_content;
-            break;
-        case 'inside-featured-image':
-            $content = $displayed_content . $content;
-            break;
+            case 'floating-left':
+                $content .= $displayed_content . '<script>
+                                jQuery(document).ready(function($) {
+                                    $(".social-share-wrapper").css({
+                                        "position": "fixed",
+                                        "left": "0",
+                                        "top": "50%",
+                                        "transform": "translateY(-50%)",
+                                        "display": "flex",
+                                        "flex-direction": "column",
+                                        "align-items": "center",
+                                    });
+                                });
+                            </script>';
+                break;
+                case 'inside-featured-image':
+                    $content .= $displayed_content . '<script>
+                                    jQuery(document).ready(function($) {
+                                        $("img.wp-post-image").css({
+                                            "position": "relative",
+                                        });
+                                        $(".social-share-wrapper").css({
+                                            "position": "absolute",
+                                            "top": "50%",
+                                            "opacity": "0.5",
+                                            "left": "50%",
+                                            "transform": "translate(-50%, -50%)",
+                                        });
+                                    });
+                                </script>';
+                    break;
         default:
             break;
     }
+
+        if (strpos($content, '[social_share_icons]') !== false) {
+            $content = str_replace('[social_share_icons]', social_share_icons_shortcode(), $content);
+        }
 
     return $content;
 }
 
 add_filter("the_content", "add_social_share_icons");
 
-// Initialize color picker script
 function initialize_color_picker_script() {
     ?>
 <script>
